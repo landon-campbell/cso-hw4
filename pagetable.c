429eb2a37ff02c643d3e621b3731ff07d6066318
Landon Campbell <psz4kk@virginia.edu>
2024-10-22 20:49:57 -0400

    added multi-level page table translate

--- pagetable.c
+++ pagetable.c
@@ -8,32 +8,52 @@ size_t ptbr; // Definition of ptbr
 
 size_t translate(size_t va){
 
-    // get the page number using bit shifting
-    size_t page_number = va >> POBITS;
-
-    // get the offset using bitwise AND
-    size_t offset = va & ((1 << POBITS) - 1);
-
-    // get the page table index using bitwise AND
-    size_t page_table_index = page_number & ((1 << (LEVELS * POBITS)) - 1);
-
-    // get the page table entry using the page table index
-    size_t page_table_entry = ((size_t *) ptbr)[page_table_index];
-
-    // get the physical page number using bit shifting
-    size_t physical_page_number = page_table_entry >> POBITS;
-
-    // get the physical address using bit shifting and bitwise OR
-    size_t physical_address = (physical_page_number << POBITS) | offset;
-
-    // check if the valid bit is 1
-    if (page_table_entry & 1){
-        // return physical address is valid bit is 1
-        return physical_address;
+    if (ptbr == 0){
+        // no page table, return all 1 bits
+        return (size_t) ~0ULL;
     }
 
-    // valid bit is 0
-    return (size_t) ~0L;
+    // get the offset using bitwise AND
+    size_t offset = va & ((1ULL << POBITS) - 1);
+
+    size_t indicies[LEVELS];
+    size_t truncated_va = va >> POBITS;
+
+    // get the indicies using bit shifting
+    for (int i = LEVELS - 1; i >= 0; i--){
+        indicies[i] = truncated_va & ((1ULL << POBITS) - 1);
+        truncated_va = truncated_va >> POBITS;
+    }
+
+    // get the page table using the page table base register
+    size_t *page_table = (size_t *) ptbr;
+
+    // get the page table entry using the indicies
+    for (int i = 0; i < LEVELS; i++){
+        size_t page_table_entry = page_table[indicies[i]];
+
+        size_t flg = page_table_entry & ((1ULL << POBITS) - 1);
+        size_t next_level_address = page_table_entry & ~((1ULL << POBITS) - 1);
+
+        // check for invalid bit
+        if (!(flg & 1ULL)){
+            // invalid and return all 1 bits
+            return (size_t) ~0ULL;
+        }
+
+        // if at last level, return physical address
+        if (i == LEVELS - 1){
+            // get the physical address using bit shifting and bitwise OR
+            return (size_t) next_level_address | offset;
+        }
+        // not on last level, and should get the next level page table
+        else {
+            page_table = (size_t *) next_level_address;
+        }
+    }
+    
+    // if something went wrong, return all 1 bits
+    return (size_t) ~0ULL;
 }
 
 void page_allocate(size_t va){
