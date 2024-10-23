867363bb4b5267e3b4f9595c732163e87fd2ecf4
Landon Campbell <psz4kk@virginia.edu>
2024-10-22 20:30:59 -0400

    added page_allocate method to pagetable.c:

--- pagetable.c
+++ pagetable.c
@@ -36,7 +36,57 @@ size_t translate(size_t va){
     return (size_t) ~0L;
 }
 
-
-
 void page_allocate(size_t va){
+
+    if (ptbr == 0){
+        // allocate memory for the page table
+        size_t page_table_size = (1 << POBITS) * sizeof(size_t);
+        int ret = posix_memalign((void **)&ptbr, (1 << POBITS), page_table_size);
+
+        // ensure that posix_memalign was successful
+        if (ret != 0){
+            fprintf(stderr, "posix_memalign failed\n");
+            exit(1);
+        }
+        
+        // set all entries in the page table to 0
+        memset((void *) ptbr, 0, page_table_size);
+    }
+
+    // get the page number using bit shifting
+    size_t page_number = va >> POBITS;
+    size_t offset = va & ((1 << POBITS) - 1);
+
+    // get the page table index using bitwise AND
+    size_t page_table_index = page_number & ((1 << (LEVELS * POBITS)) - 1);
+
+    size_t *page_table = (size_t *) ptbr;
+    // get the page table entry using the page table index
+    size_t page_table_entry = page_table[page_table_index];
+
+    // check if the valid bit is 1
+    if (page_table_entry & 1){
+        // page is already allocated
+        return;
+    }
+
+    // allocate memory for the page
+    size_t physical_page_size = (1 << POBITS);
+    void *physical_page;
+    int ret = posix_memalign(&physical_page, (1 << POBITS), physical_page_size);
+
+    // ensure that posix_memalign was successful
+    if (ret != 0){
+        fprintf(stderr, "posix_memalign failed\n");
+        exit(1);
+    }
+
+    // get physical page number (PPN)
+    size_t physical_page_number = ((size_t) physical_page) >> POBITS;
+
+    // set the page table entry
+    page_table_entry = (physical_page_number << POBITS) | 1;
+
+    // set the page table entry in the page table
+    page_table[page_table_index] = page_table_entry;
 }
