e59b0f5b1d55d60a811dc5c285ad4003e2890b8f
Landon Campbell <psz4kk@virginia.edu>
2024-10-25 20:09:21 -0400

    considered all comments from cso lab and updated code

--- pagetable.c
+++ pagetable.c
@@ -7,9 +7,11 @@
 
 size_t ptbr; // Definition of ptbr
 
-size_t translate(size_t va){
+//NOTE: For makefile, not sure if it's required but I believe the lib should be in .a format
 
-    if (ptbr == 0){
+size_t translate(size_t va) {
+
+    if (ptbr == 0) {
         // no page table, return all 1 bits
         return (size_t) ~0ULL;
     }
@@ -17,36 +19,36 @@ size_t translate(size_t va){
     // get the offset using bitwise AND
     size_t offset = va & ((1ULL << POBITS) - 1);
 
-    size_t indicies[LEVELS];
+    size_t page_table_level_indicies[LEVELS];
     size_t truncated_va = va >> POBITS;
 
     // determine num bits per index
-    size_t bits_per_index = POBITS - 3;
+    size_t bits_per_index = POBITS - 3; // 3 is log2(8)
 
-    // get the indicies using bit shifting
-    for (int i = LEVELS - 1; i >= 0; i--){
-        indicies[i] = truncated_va & ((1ULL << POBITS) - 1);
+    // get the page_table_level_indicies using bit shifting
+    for (int i = LEVELS - 1; i >= 0; i -= 1) {
+        page_table_level_indicies[i] = truncated_va & ((1ULL << POBITS) - 1);
         truncated_va = truncated_va >> bits_per_index;
     }
 
     // get the page table using the page table base register
     size_t *page_table = (size_t *) ptbr;
 
-    // get the page table entry using the indicies
-    for (int i = 0; i < LEVELS; i++){
-        size_t page_table_entry = page_table[indicies[i]];
+    // get the page table entry using the page_table_level_indicies
+    for (int i = 0; i < LEVELS; i += 1) {
+        size_t page_table_entry = page_table[page_table_level_indicies[i]];
 
         size_t valid_bit = page_table_entry & 1ULL;
         size_t next_level_address = page_table_entry & ~(1ULL);
 
         // check for invalid bit
-        if (!(valid_bit)){
+        if (!(valid_bit)) {
             // invalid and return all 1 bits
             return (size_t) ~0ULL;
         }
 
         // if at last level, return physical address
-        if (i == LEVELS - 1){
+        if (i == LEVELS - 1) {
             // get the physical address using bit shifting and bitwise OR
             return (size_t) next_level_address | offset;
         }
@@ -62,13 +64,13 @@ size_t translate(size_t va){
 
 void page_allocate(size_t va){
 
-    if (ptbr == 0){
+    if (ptbr == 0) {
         // allocate memory for the page table
         size_t page_table_size = (1ULL << POBITS);
         int ret = posix_memalign((void **)&ptbr, page_table_size, page_table_size);
 
         // ensure that posix_memalign was successful
-        if (ret != 0){
+        if (ret != 0) {
             fprintf(stderr, "posix_memalign failed\n");
             exit(1);
         }
@@ -77,39 +79,39 @@ void page_allocate(size_t va){
         memset((void *) ptbr, 0, page_table_size);
     }
 
-    size_t indicies[LEVELS];
+    size_t page_table_level_indicies[LEVELS];
     size_t truncated_va = va >> POBITS;
 
     // determine num bits per index
-    size_t bits_per_index = POBITS - 3;
+    size_t bits_per_index = POBITS - 3; // 3 is log2(8)
 
-    // get the indicies for each level using bit shifting
-    for (int i = LEVELS - 1; i >= 0; i--){
-        indicies[i] = truncated_va & ((1ULL << bits_per_index) - 1);
+    // get the page_table_level_indicies for each level using bit shifting
+    for (int i = LEVELS - 1; i >= 0; i -= 1) {
+        page_table_level_indicies[i] = truncated_va & ((1ULL << bits_per_index) - 1);
         truncated_va = truncated_va >> bits_per_index;
     }
 
     // get the page table using the page table base register
     size_t *page_table = (size_t *) ptbr;
 
-    // get the page table entry using the indicies
-    for (int i = 0; i < LEVELS; i++){
-        size_t page_table_entry = page_table[indicies[i]];
+    // get the page table entry using the page_table_level_indicies
+    for (int level = 0; level < LEVELS; level += 1) {
+        size_t page_table_entry = page_table[page_table_level_indicies[level]];
 
         size_t valid_bit = page_table_entry & 1ULL;
         size_t next_level_address = page_table_entry & ~(1ULL);
 
         // check if the valid bit is invalid, and allocate memory for the page table
-        if (!(valid_bit)){
+        if (!(valid_bit)) {
             // if only the last level, allocate memory for the PP
-            if (i == LEVELS - 1){
+            if (level == LEVELS - 1) {
                 // define page table size and allocate memory using memalign
                 void *physical_page;
                 size_t page_table_size = (1ULL << POBITS);
                 int ret = posix_memalign(&physical_page, page_table_size, page_table_size);
 
                 // ensure that posix_memalign was successful
-                if (ret != 0){
+                if (ret != 0) {
                     fprintf(stderr, "posix_memalign failed\n");
                     exit(1);
                 }
@@ -125,7 +127,7 @@ void page_allocate(size_t va){
                 int ret = posix_memalign(&next_level_page_table, page_table_size, page_table_size);
 
                 // ensure that posix_memalign was successful
-                if (ret != 0){
+                if (ret != 0) {
                     fprintf(stderr, "posix_memalign failed\n");
                     exit(1);
                 }
@@ -137,16 +139,16 @@ void page_allocate(size_t va){
             // page table entry to be updated
             page_table_entry = next_level_address | 1ULL;
             // set the page table entry in the page table
-            page_table[indicies[i]] = page_table_entry;
+            page_table[page_table_level_indicies[i]] = page_table_entry;
         }
 
         // not on last level, get the next level page table
-        if (i != LEVELS - 1){
+        if (level != LEVELS - 1) {
             page_table = (size_t *) next_level_address;
         }
         // on last level, return
         else {
-            return;
+            return; 
         }
     }
 }
